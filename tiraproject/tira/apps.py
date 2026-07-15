fa3c493a3ffc6e447c008ebfd12187ad1fbf048e
vidhinagar2005 <viddhinagar@gmail.com>
2026-07-15 15:04:14 +0530

    add

--- tiraproject/tira/apps.py
+++ tiraproject/tira/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
 
-
 class TiraConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tira'
+
+    def ready(self):
+        import os
+        if os.environ.get("RUN_MAIN") != "true":
+            return
+
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@example.com",
+                password="admin123"
+            )
